e7dabad92bfe092d04d09b5e8fa36902045e25fd
hirokinakada <dudu.hiccc@gmail.com>
2020-06-08 10:27:32 +0000

    book7

---
+++ Bookers7/app/models/book.rb
@@ -0,0 +1,42 @@
+class Book < ApplicationRecord
+	belongs_to :user
+	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+	#presence trueは空欄の場合を意味する。
+	validates :title, presence: true
+	validates :body, presence: true, length: {maximum: 200}
+
+
+
+	has_many :favorites, dependent: :destroy
+	def favorited_by?(user)
+	favorites.where(user_id: user.id).exists?
+	end
+
+	has_many :book_comments, dependent: :destroy
+
+
+
+def Book.search(search, user_or_book, how_search)
+if how_search == "1"
+            Book.where(['title LIKE ?', "%#{search}%"])
+        elsif how_search == "2"
+            Book.where(['title LIKE ?', "%#{search}"])
+        elsif how_search == "3"
+            Book.where(['title LIKE ?', "#{search}%"])
+        elsif how_search == "4"
+            Book.where(['title LIKE ?', "#{search}"])
+  else
+     Book.all
+  end
+end
+
+=begin
+def Book.search(search, user_or_book)
+  if user_or_book == "2"
+     Book.where(['title LIKE ?', "%#{search}%"])
+  else
+     Book.all
+  end
+=end
+
+end
